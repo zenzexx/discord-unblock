1260c3daf5e2a1cb6fe0c25adfde53348d3e6367
heavennborn <engoneluv@gmail.com>
2024-11-04 01:41:01 +0400

    bypass fixes

---
+++ create_autostart_yt_dc.bat
@@ -0,0 +1,42 @@
+::[Bat To Exe Converter]
+::
+::fBE1pAF6MU+EWHzeyFIiJxFRTxCRAG+zFbYT+uy2+uSFrkIeX947cZzY1aaKH/kc/1Hxd5MSw3RYlMQPB1VdZhfL
+::YAwzoRdxOk+EWAnk
+::fBw5plQjdG8=
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSjk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCqDJEmW+0g1Kw9HcACLL2i1FLFR/eb54+WEqXEZXfE8d53f5a2BNfQG6EDbYJgv2nVZk/QNGQ9XbQaqfBt6rHZH1g==
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+@echo off
+mkdir "%appdata%\discord_unblock"
+copy "%~dp0discord_youtube_unblock.exe" "%appdata%\discord_unblock\discord_youtube_unblock.exe"
+powershell -command "$s=(New-Object -COM WScript.Shell).CreateShortcut('%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\discord_unblock.lnk'); $s.TargetPath='%appdata%\discord_unblock\discord_youtube_unblock.exe'; $s.WorkingDirectory='%appdata%\discord_unblock'; $s.Save()"
+start "" /D "%appdata%\discord_unblock\" discord_youtube_unblock.exe
+echo Done.
+timeout /t 5 /nobreak
+exit
---
+++ discord_youtube_unblock.bat
@@ -0,0 +1,49 @@
+::[Bat To Exe Converter]
+::
+::fBE1pAF6MU+EWHzeyFIiJxFRTxCRAG+zFbYT+uy2+uSFrkIeX947cZzY1aaKH/Ud6Enrdp1j02Jf+A==
+::fBE1pAF6MU+EWHzeyFIiJxFRTxCRAG+zFbYT+uy2+uSFrkIeX94ocYHMyfqLOOVz
+::fBE1pAF6MU+EWHzeyFIiJxFRTxCRAG+zFbYT+uy2+uSFrkIeX948YYjM07rfbuQf5iU=
+::fBE1pAF6MU+EWHzeyFIiJxFRTxCRAG+zFbYT+uy2+uSFrkIeX94IcYH/06KLMvRd7knoFQ==
+::fBE1pAF6MU+EWHzeyFIiJxFRTxCRAG+zFbYT+uy2+uSFrkIeX94ubYbY5b2AKfQa60nbYoE66X1Vl8wACSRbcR/lbAY6yQ==
+::fBE1pAF6MU+EWHzeyFIiJxFRTxCRAG+zFbYT+uy2+uSFrkIeX94rdJzk2biHJe4H4kDoeZkSwW1Np8wDAxxUey2oYQJ6q2dM1g==
+::fBE1pAF6MU+EWHzeyFIiJxFRTxCRAG+zFbYT+uy2+uSFrkIeX94IcYH/06KLMvRFvgv3bIVN
+::fBE1pAF6MU+EWHzeyFIiJxFRTxCRAG+zFbYT+uy2+uSFrkIeX94zcZzPl7CHM+Mc+EGqYY45tg==
+::fBE1pAF6MU+EWHzeyFIiJxFRTxCRAG+zFbYT+uy2+uSFrkIeX94zcZzPl62BNfQG6ECqYY45tg==
+::YAwzoRdxOk+EWAnk
+::fBw5plQjdG8=
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSzk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+IeA==
+::cxY6rQJ7JhzQF1fEqQJiSA==
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCqDJEmW+0g1Kw9HcACLL2i1FLFR/eb54+WEqXEZXfE8d53f5a2BNfQG6EDbYJgv2nVZk4UJFB44
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+winws.exe zapret: http,https,quic ^
+--wf-tcp=80,443 --wf-udp=443,50000-50099 ^
+--filter-tcp=443 --hostlist="%~dp0list-youtube.txt" --dpi-desync=fake,split2 --dpi-desync-repeats=11 --dpi-desync-fooling=md5sig --dpi-desync-fake-tls="%~dp0tls_clienthello_www_google_com.bin" --new ^
+--filter-tcp=80,443 --dpi-desync=fake,disorder2 --dpi-desync-autottl=2 --dpi-desync-fooling=md5sig --new ^
+--filter-udp=50000-50099 --ipset="%~dp0list-discord.txt" --dpi-desync=fake --dpi-desync-repeats=6 --dpi-desync-any-protocol --dpi-desync-cutoff=n4 --new ^
+--filter-udp=443 --hostlist="%~dp0list-youtube.txt" --dpi-desync=fake --dpi-desync-repeats=11 --dpi-desync-fake-quic="%~dp0quic_initial_www_google_com.bin" --new ^
+--filter-udp=443 --dpi-desync=fake --dpi-desync-repeats=11
